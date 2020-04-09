0eae6aa7c801cc5655e1f437c3a86811a7c75917
Sigarev Aleksei <afakusima@mail.ru>
2020-04-09 12:26:01 +0300

    Added Gretting Activity

--- app/src/main/java/ru/samsung/itshool/memandos/MainActivity.kt
+++ app/src/main/java/ru/samsung/itshool/memandos/MainActivity.kt
@@ -1,12 +1,22 @@
 package ru.samsung.itshool.memandos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG : String = MainActivity::class.java.name;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val timer : Timer = Timer()
+        val intent : Intent = Intent(this, LoginActivity::class.java)
+        timer.schedule ( 800L) {  startActivity(intent) }
     }
 }
